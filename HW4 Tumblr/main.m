57935090277801ee8e66d0d3e497b6926cdd9347
Sophie Tang <sophietang@measurefrom-lm.att.net>
2014-06-26 22:38:03 -0700

    Initial Commit

---
+++ HW4 Tumblr/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HW4 Tumblr
+//
+//  Created by Sophie Tang on 6/26/14.
+//  Copyright (c) 2014 Sophie Tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ HW4 TumblrTests/HW4_TumblrTests.m
@@ -0,0 +1,34 @@
+//
+//  HW4_TumblrTests.m
+//  HW4 TumblrTests
+//
+//  Created by Sophie Tang on 6/26/14.
+//  Copyright (c) 2014 Sophie Tang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HW4_TumblrTests : XCTestCase
+
+@end
+
+@implementation HW4_TumblrTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
